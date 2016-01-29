1f3e4d4fd70e607a6c336c4e385f2b59748f6c2d
Mahmoud Ghanem <mahmoudmghanem@Moustafas-MBP.oxford.local>
2016-01-29 22:21:06 +0000

    Mahmoud's player control and chasing

---
+++ Get Off My Spawn/Assets/Mahmoud_Testing/playerBehaviour.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using UnityEngine;
+
+
+public class playerBehaviour : MonoBehaviour
+{
+	[SerializeField] private float m_MaxSpeed = 10f;                    // The fastest the player can travel in the x axis.
+
+	private Rigidbody2D m_Rigidbody2D;
+	private bool m_FacingRight = true;  // For determining which way the player is currently facing.
+
+	private void Awake()
+	{
+		m_Rigidbody2D = GetComponent<Rigidbody2D>();
+	}
+
+
+	private void FixedUpdate()
+	{
+
+	}
+
+
+	public void Move(float movex, float movey)
+	{
+		// If crouching, check to see if the character can stand up
+
+
+
+
+
+		// Move the character
+		m_Rigidbody2D.velocity = new Vector2(movex*m_MaxSpeed, movey*m_MaxSpeed);
+
+
+
+	}
+
+
+	private void Flip()
+	{
+		// Switch the way the player is labelled as facing.
+		m_FacingRight = !m_FacingRight;
+
+		// Multiply the player's x local scale by -1.
+		Vector3 theScale = transform.localScale;
+		theScale.x *= -1;
+		transform.localScale = theScale;
+	}
+
+}
