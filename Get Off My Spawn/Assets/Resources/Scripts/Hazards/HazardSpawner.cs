31cfdcdf20b7ef8a038826a280ea41d8820acfae
Kim Barrett <kimbarrett92@gmail.com>
2016-01-29 21:09:28 +0000

    Created a hazard spawner that randomly spawns obstacles  randomly regularly

---
+++ Get Off My Spawn/Assets/Resources/Scripts/Hazards/HazardSpawner.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class HazardSpawner : MonoBehaviour {
+
+	public List<GameObject> Hazards;
+
+	public float MeanFrequency;
+	public float FrequencyVariance;
+	public float MinGap = 0.1f;
+
+	// Use this for initialization
+	void Start () {
+		if(Hazards.Count > 0)
+		{
+			StartCoroutine(SpawnRandomHazard());
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	IEnumerator SpawnRandomHazard()
+	{
+		while(true)
+		{
+			float NextPause = Mathf.Max(Utility.NextGaussianDouble(MeanFrequency, FrequencyVariance), MinGap);
+			yield return new WaitForSeconds(NextPause);
+	
+			SpawnNextObstacle();
+		}
+	}
+
+	void SpawnNextObstacle()
+	{
+		int RandomObstanceIndex = Random.Range(0, Hazards.Count);
+		Instantiate(Hazards[RandomObstanceIndex]);
+	}
+}
