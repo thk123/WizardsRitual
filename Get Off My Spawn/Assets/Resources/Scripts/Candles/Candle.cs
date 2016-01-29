eaffd159fab76b4c825168d600bf09e0afc7e60f
Kim Barrett <kimbarrett92@gmail.com>
2016-01-29 21:26:36 +0000

    Basic candles script

---
+++ Get Off My Spawn/Assets/Resources/Scripts/Candles/Candle.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Candle : MonoBehaviour {
+
+	// Use this for initialization
+	bool IsCandleLit;
+
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnCollisionEntrer2D(Collider2D Collider)
+	{
+		print("Collided")
+
+		if(Collider.tag == Utility.PlayerTag)
+		{
+			if(!IsCandleLit)
+			{
+				IsCandleLit = true;
+			}
+		}
+	}
+
+	bool GetIsCandleLit()
+	{
+		return IsCandleLit;
+	}
+}
