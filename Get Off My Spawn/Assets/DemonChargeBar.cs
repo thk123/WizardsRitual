92b31f4cb482e2936733cc45b0cc1e8d59ad582f
stur86 <simonesturniolo@gmail.com>
2016-01-30 19:06:44 +0000

    Bar vanishes when player is below it

--- Get Off My Spawn/Assets/DemonChargeBar.cs
+++ Get Off My Spawn/Assets/DemonChargeBar.cs
@@ -4,13 +4,18 @@ using System.Collections;
 
 public class DemonChargeBar : MonoBehaviour {
 
+    public float transparent_radius = 1.0f;
     public Summoner summoner;
+    public PhysicsPlayerControl player;
 
-    Image bar_image;
+    Image bar_image, frame_image;
+    Animator anim;
 
 	// Use this for initialization
 	void Awake () {
         bar_image = GetComponent<Image>();
+        frame_image = transform.FindChild("BarFrame").GetComponent<Image>();
+        anim = GetComponent<Animator>();
 	}
 
     void Start()
@@ -19,11 +24,19 @@ public class DemonChargeBar : MonoBehaviour {
         {
             summoner = GameObject.FindObjectOfType<Summoner>();
         }
+        if (player == null)
+        {
+            player = GameObject.FindObjectOfType<PhysicsPlayerControl>();
+        }
     }
 
     void Update()
     {
         bar_image.fillAmount = summoner.SummonQuality;
+        // Also fade if player is close!
+        float pos_ratio = ((Vector2)player.transform.position - (Vector2)Camera.main.ScreenToWorldPoint(transform.position)).magnitude/transparent_radius;
+        anim.SetFloat("Blend", Mathf.Clamp01(pos_ratio));
+
     }
 
 }
