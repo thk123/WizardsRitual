495cc07c6ca748d6c430a5deb0cbd3427cd07fd0
Mahmoud Ghanem <mahmoudmghanem@Moustafas-MBP.oxford.local>
2016-01-30 13:18:25 +0000

    new demon

--- Get Off My Spawn/Assets/Mahmoud_Testing/Chase_Player_SpriteSheet.cs
+++ Get Off My Spawn/Assets/Mahmoud_Testing/Chase_Player_SpriteSheet.cs
@@ -6,37 +6,42 @@ public class Chase_Player_SpriteSheet : MonoBehaviour {
 
 	public Transform target;//set target from inspector instead of looking in Update
 	public float speed = 3f;
+	public float angle = 0;
+
+	Animator anim;
 
 
 	void Start () {
-
+		anim = GetComponent<Animator>();
 	}
 
 	void Update(){
 
 		Vector3 dist = (target.position - transform.position);
-		float angle = 90- (Mathf.Atan2 (dist.x, dist.y) * Mathf.Rad2Deg);
+		angle = 90- (Mathf.Atan2 (dist.x, dist.y) * Mathf.Rad2Deg);
 		Quaternion q = Quaternion.AngleAxis(angle, Vector3.forward);
 		//transform.rotation = Quaternion.RotateTowards(transform.rotation, q, 90*Time.deltaTime);
 
 		//rotate to look at the player
 		//move towards the player
+		Vector2 translation = new Vector2(0, 0);
 		if (dist.magnitude>1.5){//move if distance from target is greater than 1
-			Vector2 translation = (Vector2) (q * new Vector3(speed*Time.deltaTime, 0, 0));
+			translation = (Vector2) (q * new Vector3(speed*Time.deltaTime, 0, 0));
 
 			transform.Translate(translation );
 		}
-		if (angle > 180 && transform.localScale.x < 0) {
-			Vector3 newScale = transform.localScale;
-			newScale.x *= -1;
-			transform.localScale = newScale;
-		}
-		if (angle < 180 && transform.localScale.x > 0) {
-			Vector3 newScale = transform.localScale;
-			newScale.x *= -1;
-			transform.localScale = newScale;
-		}
 
+		if (Mathf.Abs (translation.y) > Mathf.Abs (translation.x)) {
+			if (translation.y < 0) {
+				anim.SetInteger ("Direction", 2);
+			} else {
+				anim.SetInteger ("Direction", 1);
+			}
+		}
+			
+		else if (Mathf.Abs (translation.y) < Mathf.Abs (translation.x)){
+			anim.SetInteger ("Direction", 0);
+		}
 
 	}
 
