1c26a762d6421de8bb0d7a5cd8ab1b76bd970b0d
Kim Barrett <kimbarrett92@gmail.com>
2016-01-30 01:45:47 +0000

    Candles only get lit in the right order

--- Get Off My Spawn/Assets/CandlePatternGenerator.cs
+++ Get Off My Spawn/Assets/CandlePatternGenerator.cs
@@ -4,10 +4,21 @@ using System.Collections.Generic;
 
 public class CandlePatternGenerator : MonoBehaviour {
 
-    struct candle_pos
+    public struct candle_pos
     {
         public int circle;
         public int candle;
+
+        public candle_pos(int CircleIndex, int ClockPosition)
+        {
+            circle = CircleIndex;
+            candle = ClockPosition;
+        }
+
+        public override string ToString()
+        {
+            return circle + ":" + candle;
+        }
     }
 
     public float tot_radius = 3.0f;
@@ -16,14 +27,18 @@ public class CandlePatternGenerator : MonoBehaviour {
     public Color circle_color;
     public float circle_width;
     public Material circle_material;
-    public GameObject candle_prefab;
+    public Candle candle_prefab;
 
     List<List<Vector2>> pattern;
     LineRenderer[] circle_rends;
-    Queue<candle_pos> correct_sequence;
+    Queue<Candle> correct_sequence;
+
+    List<Candle> Candles;
 
 	// Use this for initialization
 	void Awake () {
+
+        Candles = new List<Candle>();
 
         int[] circle_sizes = new int[circle_num];
         circle_rends = new LineRenderer[circle_num];
@@ -47,44 +62,48 @@ public class CandlePatternGenerator : MonoBehaviour {
         // Now assign positions
 
         pattern = new List<List<Vector2>>();
-        for (int i = 0; i < circle_num; ++i)
+        for (int circle_index = 0; circle_index < circle_num; ++circle_index)
         {
-            float r = tot_radius / circle_num * (i+1);
+            float r = tot_radius / circle_num * (circle_index+1);
             float phi = Mathf.PI * 2.0f * Random.value;
             pattern.Add(new List<Vector2>());
-            for (int j = 0; j < circle_sizes[i]; ++j)
+            for (int j = 0; j < circle_sizes[circle_index]; ++j)
             {
-                pattern[i].Add(new Vector2(r * Mathf.Cos(Mathf.PI * 2.0f / circle_sizes[i] * j + phi),
-                                           r * Mathf.Sin(Mathf.PI * 2.0f / circle_sizes[i] * j + phi)));
+                pattern[circle_index].Add(new Vector2(r * Mathf.Cos(Mathf.PI * 2.0f / circle_sizes[circle_index] * j + phi),
+                                           r * Mathf.Sin(Mathf.PI * 2.0f / circle_sizes[circle_index] * j + phi)));
 
-                GameObject candle = Instantiate(candle_prefab);
+                Candle candle = Instantiate(candle_prefab);
                 candle.transform.SetParent(transform);
-                candle.transform.localPosition = (Vector3)pattern[i][j];
+                candle.transform.localPosition = (Vector3)pattern[circle_index][j];
+                candle.SetCandlePosition(new candle_pos(circle_index, j));
+                candle.OnCandleLit += Candle_OnCandleLit;
+                Candles.Add(candle);
+
             }
             
             // Now draw the circle
-            GameObject circle_draw = new GameObject("circle_" + (i+1));
+            GameObject circle_draw = new GameObject("circle_" + (circle_index+1));
             circle_draw.transform.SetParent(transform);
             circle_draw.transform.localPosition = Vector3.zero;
-            circle_rends[i] = circle_draw.AddComponent<LineRenderer>();
-            circle_rends[i].useWorldSpace = false;
-            circle_rends[i].SetVertexCount(circle_sizes[i]+1);
-            circle_rends[i].SetColors(circle_color, circle_color);
-            circle_rends[i].SetWidth(circle_width, circle_width);
-            circle_rends[i].material = circle_material;
-            circle_rends[i].sortingLayerName = "Circle";
-            for (int j = 0; j <= circle_sizes[i]; ++j)
+            circle_rends[circle_index] = circle_draw.AddComponent<LineRenderer>();
+            circle_rends[circle_index].useWorldSpace = false;
+            circle_rends[circle_index].SetVertexCount(circle_sizes[circle_index]+1);
+            circle_rends[circle_index].SetColors(circle_color, circle_color);
+            circle_rends[circle_index].SetWidth(circle_width, circle_width);
+            circle_rends[circle_index].material = circle_material;
+            circle_rends[circle_index].sortingLayerName = "Circle";
+            for (int j = 0; j <= circle_sizes[circle_index]; ++j)
             {
-                if (j < circle_sizes[i])
-                    circle_rends[i].SetPosition(j, (Vector3)pattern[i][j]);
+                if (j < circle_sizes[circle_index])
+                    circle_rends[circle_index].SetPosition(j, (Vector3)pattern[circle_index][j]);
                 else
-                    circle_rends[i].SetPosition(j, (Vector3)pattern[i][0]);
+                    circle_rends[circle_index].SetPosition(j, (Vector3)pattern[circle_index][0]);
             }
 
         }
 
         // And generate the correct sequence!
-        List<candle_pos> ordered_sequence = new List<candle_pos>();
+        /*List<candle_pos> ordered_sequence = new List<candle_pos>();
         candle_pos cpos = new candle_pos();
 
         for (int i = 0; i < circle_num; ++i)
@@ -104,11 +123,48 @@ public class CandlePatternGenerator : MonoBehaviour {
             ordered_sequence[i] = ordered_sequence[iswap];
             ordered_sequence[iswap] = cpos;
         }
-        correct_sequence = new Queue<candle_pos>(ordered_sequence);
+        correct_sequence = new Queue<candle_pos>(ordered_sequence);*/
+
+        //List<candle_pos> ordered_sequence = new List<candle_pos>();
+        /*candle_pos cpos = new candle_pos();
+
+        for (int i = 0; i < circle_num; ++i)
+        {
+            for (int j = 0; j < circle_sizes[i]; ++j)
+            {
+                cpos.circle = i;
+                cpos.candle = j;
+                ordered_sequence.Add(cpos);
+            }
+        }*/
+        // Now shuffle the ordered sequence
+        for (int i = 0; i < Candles.Count; ++i)
+        {
+            int iswap = Random.Range(i, Candles.Count);
+            Candle cpos = Candles[i];
+            Candles[i] = Candles[iswap];
+            Candles[iswap] = cpos;
+        }
+        correct_sequence = new Queue<Candle>(Candles);
     }
 	
 	// Update is called once per frame
 	void Update () {
-	
 	}
+
+    void Candle_OnCandleLit(Candle sender)
+    {
+        print("Candle lit: " + sender.CandlePosition.ToString());
+        if(correct_sequence.Peek() == sender)
+        {
+            print("    The correct candle");
+            Candle next_candle = correct_sequence.Dequeue();
+        }
+        else
+        {
+            print("    Incorrect candle");
+            sender.SetCandleLit(false);
+            // TODO: knock some quality off the summoning
+        }
+    }
 }
