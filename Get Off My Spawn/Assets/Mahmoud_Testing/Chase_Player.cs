b19650adeec96b86f8073fbe5b66ef012ff8d289
Mahmoud Ghanem <mahmoudmghanem@Moustafas-MBP.oxford.local>
2016-01-30 16:38:08 +0000

    dog

--- Get Off My Spawn/Assets/Mahmoud_Testing/Chase_Player.cs
+++ Get Off My Spawn/Assets/Mahmoud_Testing/Chase_Player.cs
@@ -6,25 +6,41 @@ public class Chase_Player : MonoBehaviour {
 
 	public Transform target;//set target from inspector instead of looking in Update
 	public float speed = 3f;
-
+	bool jumping = false;
 
 	void Start () {
 
 	}
 
 	void Update(){
+		if (jumping) {
+			jump ();
+		} else {
+			chase ();
+		}
+			
+	}
 
+	void chase(){
 		Vector3 dist = (target.position - transform.position);
-		float angle = 90- (Mathf.Atan2 (dist.x, dist.y) * Mathf.Rad2Deg);
-		Quaternion q = Quaternion.AngleAxis(angle, Vector3.forward);
+		float angle = 90 - (Mathf.Atan2 (dist.x, dist.y) * Mathf.Rad2Deg);
+		Quaternion q = Quaternion.AngleAxis (angle, Vector3.forward);
 		transform.rotation = q;
+
+	
 
 		//rotate to look at the player
 		//move towards the player
-		if (dist.magnitude>1.5){//move if distance from target is greater than 1
-			transform.Translate(new Vector2(speed* Time.deltaTime,0) );
-		}
+		if (dist.magnitude > 1.5) {//move if distance from target is greater than 1
+			transform.Translate (new Vector2 (speed * Time.deltaTime, 0));
 
+		} else {
+			jumping = true;
+		}
+	}
+
+	void jump() {
+		transform.Translate (new Vector2 (speed*4 * Time.deltaTime, 0));
 	}
 
 }
