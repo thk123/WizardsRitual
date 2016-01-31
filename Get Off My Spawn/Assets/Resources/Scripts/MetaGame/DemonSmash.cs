a7c3f6c0d8fdbf597a7d5d65d579b9b2274369a2
Kim Barrett <kimbarrett92@gmail.com>
2016-01-31 02:00:15 +0000

    Starting work on the demon smash stuff

---
+++ Get Off My Spawn/Assets/Resources/Scripts/MetaGame/DemonSmash.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DemonSmash : MonoBehaviour {
+
+	public float HitDamage;
+	public float HitPeriod;
+
+	Building TargetBuilding;
+	IEnumerator HitBuildingCoroutine;
+
+	// Use this for initialization
+	void Start () {
+		TargetBuilding = null;
+		HitBuildingCoroutine = null;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+
+	void OnTriggerEnter2D(Collider2D collid)
+	{
+		if(collid.tag == Utility.BuildingsTag)
+		{
+			TargetBuilding = collid.GetComponent<Building>();
+			HitBuildingCoroutine = HitBuilding();
+			StartCoroutine(HitBuildingCoroutine);
+		}
+	}
+
+	void OnTriggerLeave2D(Collider2D collid)
+	{
+		if(collid.GetComponent<Building>() == TargetBuilding)
+		{
+			StopCoroutine(HitBuildingCoroutine);
+		}
+	}
+
+	IEnumerator HitBuilding()
+	{
+		while(TargetBuilding != null)
+		{
+			TargetBuilding.Hit(HitDamage);
+
+			yield return new WaitForSeconds(HitPeriod);
+		}
+		//return null;
+	}
+}
