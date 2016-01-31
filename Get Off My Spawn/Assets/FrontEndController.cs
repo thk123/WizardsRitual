62fca6ea49eabbf558ff5c2ef7031dbf8ab76390
Kim Barrett <kimbarrett92@gmail.com>
2016-01-31 13:03:45 +0000

    Basic front end junping to main game

---
+++ Get Off My Spawn/Assets/FrontEndController.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FrontEndController : MonoBehaviour {
+
+	public string MainGameLevel;
+	public string CreditsLevel;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void StartGame()
+	{
+		Application.LoadLevel(MainGameLevel);
+	}
+
+	public void Credits()
+	{
+		Application.LoadLevel(CreditsLevel);
+	}
+
+	public void ExitGame()
+	{
+		Application.Quit();
+	}
+
+}
