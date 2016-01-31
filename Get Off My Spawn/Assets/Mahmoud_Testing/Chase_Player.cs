5d295e30e9beb01e8b7014b278705f653258e455
Kim Barrett <kimbarrett92@gmail.com>
2016-01-31 10:58:43 +0000

    Made the dog flip when the player is on the other side

--- Get Off My Spawn/Assets/Mahmoud_Testing/Chase_Player.cs
+++ Get Off My Spawn/Assets/Mahmoud_Testing/Chase_Player.cs
@@ -11,9 +11,10 @@ public class Chase_Player : MonoBehaviour {
 	public float jumpDistance = 1.5f;
 	public float prepProgress = 0;
 
+	Vector3 StartingScale;
 
 	void Start () {
-
+		StartingScale = transform.localScale;
 	}
 
 	void Update(){
@@ -36,15 +37,15 @@ public class Chase_Player : MonoBehaviour {
 
 	void chase(){
 		Vector3 dist = (target.position - transform.position);
-		float angle = 90 - (Mathf.Atan2 (dist.x, dist.y) * Mathf.Rad2Deg);
-		Quaternion q = Quaternion.AngleAxis (angle, Vector3.forward);
+		float DirectionMultiplier;
+		Quaternion q = UpdateRotation(dist, out DirectionMultiplier);
 		transform.rotation = q;
 	
 
 		//rotate to look at the player
 		//move towards the player
 		if (dist.magnitude > jumpDistance) {//move if distance from target is greater than 1
-			transform.Translate (new Vector2 (speed * Time.deltaTime, 0));
+			transform.Translate (new Vector2 (speed * Time.deltaTime * DirectionMultiplier, 0));
 
 		} else {
 			jumping = true;
@@ -59,20 +60,49 @@ public class Chase_Player : MonoBehaviour {
 			GetComponent<AudioSource>().Play();
 		} else {
 			Vector3 dist = (target.position - transform.position);
-			float angle = 90 - (Mathf.Atan2 (dist.x, dist.y) * Mathf.Rad2Deg);
-			Quaternion q = Quaternion.AngleAxis (angle, Vector3.forward);
+			float DirectionMultiplier;
+			Quaternion q = UpdateRotation(dist, out DirectionMultiplier);
 			transform.rotation = Quaternion.Slerp(transform.rotation, q, Time.deltaTime);
-			transform.Translate (new Vector2 (-0.1f* speed * Time.deltaTime, 0));
+			transform.Translate (new Vector2 (-0.1f* speed * Time.deltaTime * DirectionMultiplier, 0));
+
+			FlyDirection = DirectionMultiplier;
 		}
 		
 	}
 
+	float FlyDirection;
+
 	void fly() {
 		Vector3 dist = (target.position - transform.position);
-		float angle = 90 - (Mathf.Atan2 (dist.x, dist.y) * Mathf.Rad2Deg);
-		Quaternion q = Quaternion.AngleAxis (angle, Vector3.forward);
-		transform.rotation = Quaternion.Slerp(transform.rotation, q, Time.deltaTime*0.3f);
-		transform.Translate (new Vector2 (speed*4 * Time.deltaTime, 0));
+		/*float DirectionMultiplier;
+		Quaternion q = UpdateRotation(dist, out DirectionMultiplier);
+		transform.rotation = Quaternion.Slerp(transform.rotation, q, Time.deltaTime*0.3f);*/
+		transform.Translate (new Vector2 (speed*4 * Time.deltaTime * FlyDirection, 0));
+	}
+
+	public Quaternion UpdateRotation(Vector2 dist, out float ReverseDirection)
+	{
+		if(dist.x > 0.0f)
+		{
+			transform.localScale = StartingScale;
+			float angle = 90 - (Mathf.Atan2 (dist.x, dist.y) * Mathf.Rad2Deg);
+			Quaternion q = Quaternion.AngleAxis (angle, Vector3.forward);
+			
+			ReverseDirection = 1.0f;
+
+			return q;
+		}
+		else
+		{
+			// Reverse the scale
+			transform.localScale = Vector3.Scale(StartingScale, new Vector3(-1.0f, 1.0f, 1.0f));
+			float angle = 90 - (Mathf.Atan2 (-dist.x, -dist.y) * Mathf.Rad2Deg);
+			Quaternion q = Quaternion.AngleAxis (angle, Vector3.forward);
+			
+			ReverseDirection = -1.0f;
+
+			return q;
+		}
 	}
 
 }
